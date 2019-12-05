00b0cb0a65531ad60536b4b380df6f4dfd8778cb
杨凯 <372492384@qq.com>
2019-12-05 20:59:31 +0800

    12/5-21:00

--- 003-js/js/general.js
+++ 003-js/js/general.js
@@ -34,3 +34,6 @@
 	function getScrollTop(){
 		return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 	}
+	function getScrollLeft(){
+		return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+	}
