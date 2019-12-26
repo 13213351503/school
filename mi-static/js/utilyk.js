e4e6954f5e514643c390a7dbe894e5aed0ce5848
杨凯 <372492384@qq.com>
2019-12-26 21:01:27 +0800

    12/26

--- mi-static/js/utilyk.js
+++ mi-static/js/utilyk.js
@@ -1,5 +1,8 @@
 handleCart()
 handleNav()
+handleCarousel()
+handleCate()
+handleCountDown()
 function handleCart(){
 	//获取元素
 	var oCart = document.querySelector('.top .cart')
@@ -39,28 +42,36 @@ function handleNav(){
 	var oHeaderNavContent = document.querySelector('.header .header-nav-content')
 	var oContainer = oHeaderNavContent.querySelector('.header-nav-content .container')
 	var headerTimer = 0
+	var loadTimer = 0
 	// console.log(aHeaderNav)
 	// 遍历添加事件
 	for(var i = 0;i < aHeaderNav.length - 2;i++){
-		//生成加载图标
-		oContainer.innerHTML = '<div class="loader"></div>'
-		oContainer.style.borderTop = '1px solid #ccc'
 		//鼠标移入事件
+		aHeaderNav[i].index = i
 		aHeaderNav[i].onmouseenter = function(){
+			oContainer.innerHTML = '<div class="loader"></div>'
+			oContainer.style.borderTop = '1px solid #ccc'
 			clearTimeout(headerTimer)
 			animate(oHeaderNavContent,{height:180},true,function(){
 				oContainer.style.overflow = 'visible'
-				
 			})
+			clearTimeout(loadTimer)
+			var index = this.index
+			loadTimer = setTimeout(function(){
+				loadDate(index)
+			},1000)	
 		}
 		aHeaderNav[i].onmouseleave = function(){
 			hidNavContent()
 		}
 	}
+	//生成函数使用定时器
 	function hidNavContent(){
 		headerTimer = setTimeout(function(){
 			animate(oHeaderNavContent,{height:0},true,function(){
 				oContainer.style.overflow = 'hidden'
+				oContainer.style.borderTop = 'none'
+
 			})
 		},500)
 	}
@@ -71,5 +82,106 @@ function handleNav(){
 	oHeaderNavContent.onmouseleave = function(){
 		hidNavContent()
 	}
+	function loadDate(index){
+		var date = aNavDateList[index]
+		// console.log(date)
+		// 
+		// 用数据拼接html字符，并把接好的html字符显示出来
+		var html = '<ul>'
+		for(var i = 0;i<date.length;i++){
+			html += '	<li>'
+			html +=	'		<div class="img-box">'
+			html +=	'			<a href="'+date[i].url+'">'
+			html +=	'				<img src="'+date[i].src+'" alt="">'
+			html +=	'			</a>'
+			html +=	'		</div>'
+			html +=	'		<p class="product-name">'+date[i].name+'</p>'
+			html +=	'		<p class="product-price">'+date[i].price+'元起</p>'
+			if(date[i].tag){
+				html +=	'		<span class="tag">'+date[i].tag+'</span>'
+			}
+			html +=	'	</li>'
+		}
+		html += '</ul>'
+		oContainer.innerHTML = html
+	}
 	
+}
+function handleCarousel(){
+	new Carousel({
+		id:'carousel',
+		aImg:['images/b1.jpg','images/b2.jpg','images/b3.jpg'],
+		width:1226,
+		height:460,
+		playDuration:5000
+	})
+}
+//处理分类面板
+function handleCate(){
+	var oCateBox = document.querySelector('.cate-box')
+	var oCateContent = document.querySelector('.cate-content')
+	var aCateItem = oCateBox.querySelectorAll('.cate-item')
+	// console.log(aCateItem)
+	for(var i = 0;i<aCateItem.length;i++){
+		aCateItem[i].index = i
+		aCateItem[i].onmouseenter = function(){
+			for(var j =0;j<aCateItem.length;j++){
+				aCateItem[j].className = 'cate-item'
+			}
+			this.className = 'cate-item active'
+			oCateContent.style.display = 'block'
+
+			loadCade(this.index)
+		}
+		
+	}
+	oCateBox.onmouseleave = function(){
+		for(var j =0;j<aCateItem.length;j++){
+				aCateItem[j].className = 'cate-item'
+			}
+		oCateContent.style.display = 'none'
+	}
+
+	function loadCade(index){
+		console.log(index)
+		var date = aLoadCadeList[index]
+		var html = '<ul>'
+		for(var i = 0;i<date.length;i++){
+			html +=			'<li>'
+			html +=				'<a href="'+date[i].url+'">'
+			html +=					'<img src="'+date[i].img+'" alt="">'
+			html +=					'<span>'+date[i].name+'</span>'
+			html +=				'</a>'
+			html +=			'</li>'
+		}
+		html += '</ul>'
+		oCateContent.innerHTML = html
+	}
+	
+}
+function handleCountDown(){
+	var aTimerNum = document.querySelectorAll('.timer-num')
+	var timer = 0
+	var endTime = new Date('2020/1/1 12:00:00')
+	// console.log(aTimerNum)
+	time()
+	function time(){
+		var endTimer = endTime.getTime()
+		var nowTimer = Date.now()
+		var timeOver = endTimer-nowTimer
+		var miao = timeOver/1000
+		var iHour = parseInt(miao/3600)
+		var iMinter = parseInt((miao%3600)/60)
+		var iSecond = parseInt((miao%3600)%60)
+		// console.log(iSecond)
+		aTimerNum[0].innerHTML = iHour
+		aTimerNum[1].innerHTML = iMinter
+		aTimerNum[2].innerHTML = iSecond
+	}
+	setInterval(time,1000)
+	// console.log(endTimer)
+	// console.log(nowTimer)
+	// console.log(timeOver)
+	
+
 }
