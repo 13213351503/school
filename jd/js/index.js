3bacb7862bf0bd62e762d4807afeba5ba5fa3573
杨凯 <372492384@qq.com>
2020-06-07 18:11:36 +0800

    18:11

--- jd/js/index.js
+++ jd/js/index.js
@@ -1,9 +1,18 @@
+
+
+
+
+
+
 (function($){
 	// 顶部导航逻辑开始
 	$('.top .top-right .dropdown').hover(function(){
-		$(this).addClass('menu-active');
+		var menuData = $(this).data('active');
+		console.log(menuData);
+		$(this).addClass(menuData + '-active');
 	},function(){
-		$(this).removeClass('menu-active');
+		var menuData = $(this).data('active');
+		$(this).removeClass(menuData + '-active');
 	})
 
 	// 顶部导航逻辑结束
