b95b20ff0159f16f13f533269d86ab2a2f8333ec
杨凯 <372492384@qq.com>
2020-06-20 17:59:26 +0800

    18:00

--- 007-css进阶/js/rem.js
+++ 007-css进阶/js/rem.js
@@ -1,9 +1,16 @@
 ;(function(win,doc){
+	//根据不同的视口宽度设置不同的根元素字体大小
 	//1.获取根元素
-	var oRoot = document.getElementsByTagName('html')[0];
-	//2.获取适口宽度
-	var iWidth = document.body.clientWidth || document.documentElement.clientWidth;
-	//3.根据比例计算元素字体大小
-	var oWidth = iWidth / 10;
-	oRoot.style.fontSize = oWidth + 'px';
+	var oRoot = doc.getElementsByTagName('html')[0]
+	function refresh(){
+		//2.获取视口宽度
+		var width = doc.body.clientWidth || doc.documentElement.clientWidth;
+		// console.log(width)
+		//3.根据一定比例设置根元素字体大小
+		var oFsize = width / 10;
+		oRoot.style.fontSize = oFsize + 'px';
+	}
+	win.addEventListener('DOMContentLoaded',refresh,false);
+	win.addEventListener('load',refresh,false)
+	win.addEventListener('resize',refresh,false)
 })(window,document)
