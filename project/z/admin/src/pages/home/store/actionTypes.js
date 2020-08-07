f1ee03b37179c91571a178330636cd4eb2a7680c
杨凯 <372492384@qq.com>
2020-08-07 16:45:21 +0800

    16:45

--- project/z/admin/src/pages/home/store/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const GET_COUNTS = 'home@get_counts';
--- project/z/admin/src/pages/login/store/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const LOGIN_START_ACTIOIN = 'home@login_start_actioin';
-export const LOGIN_DONE_ACTIOIN = 'home@login_done_actioin';
