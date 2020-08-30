1ecc79a5fe57aa5db399b75d2010594d9b45a476
杨凯 <372492384@qq.com>
2020-08-30 21:27:04 +0800

    8/30 21:26

--- wxxcx/pages/article/article.js
+++ wxxcx/pages/article/article.js
@@ -5,7 +5,28 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        articles:[
+            {
+                author:'K',
+                avatar:'/pages/image/avatar/111.jpg',
+                time:'2020-08-30',
+                title:'火锅',
+                mainImage:'/pages/image/article/u=1412718983,1138680485&fm=26&gp=0.jpg',
+                desc:'毛肚,牛肉',
+                star:'200',
+                view:'999',
+            },
+            {
+                author:'y',
+                avatar:'/pages/image/avatar/u=1283079160,971140895&fm=26&gp=0.jpg',
+                time:'2020-08-06',
+                title:'烧烤',
+                mainImage:'/pages/image/article/u=3604995668,4286802266&fm=26&gp=0.jpg',
+                desc:'五花肉',
+                star:'300',
+                view:'899',
+            }
+        ]
     },
 
     /**
