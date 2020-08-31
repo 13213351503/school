6459608f81cbb6b2fb6973f940152569567d2431
杨凯 <372492384@qq.com>
2020-08-31 23:08:22 +0800

    8/31 23:08

--- wxxcx/pages/article/article-detail/article-detail.js
+++ wxxcx/pages/article/article-detail/article-detail.js
@@ -19,10 +19,10 @@ Page({
 
         //获取文章收藏状态
         var article_collection = wx.getStorageSync('article_collection')
-        console.log(article_collection)
+        // console.log(article_collection)
 
         if(article_collection){
-            isCollected = !!article_collection
+            isCollected = !!article_collection[detailId]
         }
         //如果没有收藏状态，初始化收藏状态
         else{
@@ -33,9 +33,18 @@ Page({
             */ 
             var data = {}
             data[detailId] = false
+            //设置storage储存文章状态
             wx.setStorageSync('article_collection', data)
         }
-
+        //处理背景音乐
+        //获取背景音乐播放实例
+        var backgroundAudioManager = wx.getBackgroundAudioManager()
+        backgroundAudioManager.onPlay(function(){
+            this.setData({isLoading:true})
+        }.bind(this))
+        backgroundAudioManager.onPause(function(){
+            this.setData({isLoading:false})
+        }.bind(this))
 
         // console.log(article)
         this.setData({...article,isCollected})
@@ -94,8 +103,23 @@ Page({
         // wx.setStorageSync('key', {name:'hadden'})
         // var value = wx.getStorageSync('key')
         // console.log(value)
+        //获取文章收藏状态
         var article_collection = wx.getStorageSync('article_collection')
-        console.log(article_collection)
+        //获取当前文章收藏状态
+        var cuurentCollection = article_collection[this.data.id]
+        // console.log(cuurentCollection)
+        //改变当前文章收藏状态
+        article_collection[this.data.id] = !cuurentCollection
+        //设置storage
+        wx.setStorageSync('article_collection', article_collection)
+        //同步改变收藏图标显示状态
+        this.setData({isCollected:!cuurentCollection},function(){
+            wx.showToast({
+                title: cuurentCollection ? '取消成功' : '收藏成功',
+                icon: 'success',
+                duration: 2000
+            })
+        })
     },
     tapaction:function(){
         var itemList = ['分享给好友','分享到朋友圈','分享到微博']
@@ -121,13 +145,17 @@ Page({
     tapMusic:function(){
         var backgroundAudioManager = wx.getBackgroundAudioManager()
         // console.log(this.data.isLoading)
-        if(this.data.isLoading){
-
+        if(this.data.isLoading){//正在播放
+            backgroundAudioManager.pause()
+            //改变播放音乐图标
+            // this.setData({isLoading:false})
         }
-        else{
+        else{//暂停状态
             backgroundAudioManager.title = this.data.music.title
             backgroundAudioManager.coverImgUrl = this.data.music.coverImgUrl
             backgroundAudioManager.src = this.data.music.src
+            //改变播放音乐图标
+            // this.setData({isLoading:true})
         }
     }
 })
